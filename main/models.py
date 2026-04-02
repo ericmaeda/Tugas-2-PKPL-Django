4662a54017e5af1adaee9f215bc2b1bcc5a4176b
Angga Ziaurrohchman <anggazia24@gmail.com>
2026-04-02 21:23:06 +0700

    ngetest simpen perubahan

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Theme(models.Model):
+    bg_color   = models.CharField(max_length=7, default='#000000')
+    card_color = models.CharField(max_length=7, default='#171717')
+    text_color = models.CharField(max_length=7, default='#FFFFFF')
+    accent     = models.CharField(max_length=7, default='#A5CDFE')
+    font       = models.CharField(max_length=50, default='Poppins')
